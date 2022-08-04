8cb7ae7bd7fc88074fc71f4f6037b725157f6d2e
Rimon <rimons2804@gmail.com>
2022-08-04 19:39:50 +0300

    added edit

---
+++ src/cmps/task-edit.vue
@@ -0,0 +1,18 @@
+<template>
+
+<h3>edit</h3>
+</template>
+ <script>
+ export default {
+ name: 'ProjectApp',
+    components: {},
+  data() {
+   return {};
+    },
+  created() {},
+ methods: {},
+ computed: {},
+ unmounted() {},
+  };
+  </script>
+ <style></style>
