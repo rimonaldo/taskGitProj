df0a6c657740a0873cd4dded7a0c4eebc56d249f
Rimon <rimons2804@gmail.com>
2022-08-04 12:50:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <app-header />
+  <app-footer />
+</template>
+
+<script>
+import appHeader from '@/cmps/app-header.vue'
+import appFooter from './cmps/app-footer.vue'
+export default {
+  components: {
+    appHeader,
+    appFooter,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {},
+  computed: {
+
+  },
+
+}
+</script>
+
+
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+// import { userStore } from './modules/user.store.js'
+import { createStore } from "vuex";
+
+const store = createStore({
+  strict: true,
+  starredBoard: {},
+  isCover: false,
+  state: {
+  
+  },
+  getters: {
+   
+  },
+  mutations: {
+    
+    
+  },
+  actions: {
+   
+  },
+  modules: {
+  },
+});
+
+export default store;
