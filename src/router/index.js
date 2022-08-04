9ecfd22b363a852b50de7e26cfd2cfc028fdc0cd
Rimon <rimons2804@gmail.com>
2022-08-04 16:11:38 +0300

    added base scss

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import appHeader from '../cmps/app-header.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-    
+      path:'/',
+      component:appHeader
     },
   ]
 })
