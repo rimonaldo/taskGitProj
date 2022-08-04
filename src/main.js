867d0bde036b37560fc9836e68b559ae5fc944a5
Rimon <rimons2804@gmail.com>
2022-08-04 12:54:14 +0300

    some mods

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/main-new.scss'
+import './styles/main.scss'
 
 const app = createApp(App)
 
